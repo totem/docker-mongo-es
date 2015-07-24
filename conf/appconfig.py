b36c9d87a6341d29b3858172bebd7901560072e9
sukrit007 <sukrit007@gmail.com>
2015-07-24 13:03:32 -0700

    Bump default timeout and make it configurable

--- conf/appconfig.py
+++ conf/appconfig.py
@@ -8,6 +8,7 @@ MONGO_INCLUDES = os.getenv('MONGO_INCLUDES', '')
 
 ES_URL = os.getenv('ES_URL', 'http://localhost:9200')
 ES_INDEXES = yaml.load(os.getenv('ES_INDEXES') or '{}')
+ES_TIMEOUT_SECONDS = int(os.getenv('ES_TIMEOUT_SECONDS', '100'))
 
 MONGO_CONNECTOR_CONFIG = 'mongo-connetor.json'
 
@@ -30,7 +31,12 @@ DEFAULTS = {
         'docManagers': [
             {
                 'docManager': 'elastic_doc_manager',
-                'targetURL': ES_URL
+                'targetURL': ES_URL,
+                "args": {
+                    "clientOptions": {
+                        "timeout": ES_TIMEOUT_SECONDS
+                    }
+                }
             }
         ]
     }
